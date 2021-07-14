32b75c26a54b68f21ead9ee19f5ad9a1e55654ce
Vanshika Singhal <vanshikasinghal69@gmail.com>
2021-07-15 00:58:16 +0530

    Update [main.js]

--- Day 15/main.js
+++ Day 15/main.js
@@ -3,3 +3,12 @@ const App = () => {
 }
 
 ReactDOM.render(React.createElement(App), document.getElementById('hello'))
+
+// ReactDom.render() has 2 arguments:
+// 1. what is the content that u want to render
+// 2. where do u want to render the content
+
+// return React.createElement(1, 2, 3) :
+// 1 - name of the element that u want to create
+// 2 - {} - for adding a class or id to the created element for styling or for adding certain event listeners like onclick, etc
+// 3 - the actual content that u want to print in the created element
